cd0db36037ed7d361ed62041ab9d0ee555216f40
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-01-06 01:15:27 +0600

    Problem : 01 advanced part solved

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let totalRows = 30;
+let totalIncome = 0;
 for (let i = 1; i <= 30; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 || i % 5 === 0) {
         console.log('row', i + ' - Premium Row ');
+        totalIncome = totalIncome + 800
     }else{
         console.log('row', i + ' - Normal Row ');
+        totalIncome += 500
     }
 
+    
+    
 }
+console.log('Total Income = ', totalIncome);
