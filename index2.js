9cba314e95b1494762ddd8b594cbaa19506df6b1
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-01-06 16:51:20 +0600

    Problem 3 with advance done

--- index2.js
+++ index2.js
@@ -5,9 +5,17 @@ let employe = {
     employeeId: 101,
     departmentCode: "HR"
 }
+let threeChar = ''
 
-let threeChar = employe.lastName.slice(0, 3).toLowerCase()
-let result = threeChar + employe.employeeId + "@" + companyName.toLowerCase() + ".com"
+if (employe.lastName.length < 3) {
+    threeChar = threeChar + lastName
+} else {
+    threeChar = employe.lastName.slice(0, 3).toLowerCase()
+}
+let UserId = threeChar + employe.employeeId + "@" + companyName.toLowerCase() + ".com"
+let passWord = employe.firstName.slice(-3).toLowerCase() + "#" + employe.departmentCode
 
-console.log(result);
+console.log("ID:" + UserId);
+
+console.log("passWord:" + passWord);
 
