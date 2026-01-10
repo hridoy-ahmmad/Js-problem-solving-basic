4f088552728569e3a316f45aef8d6ac76681eaa4
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-01-10 16:15:23 +0600

    number palindrome

--- index4.js
+++ index4.js
@@ -1,14 +1,18 @@
 // Check Palindrome
 
-function palindrome(word){
-    const reversed = word.split('').reverse().join('')
-    if(word === reversed){
-        return true
-    }else{
+function palindrome(word) {
+    const str = word.toString()
+    const reversed = str.split('').reverse().join('')
+    if (str === reversed) {
+        return word
+    }
+    else {
         return false
     }
+    // return str === reversed
 }
 console.log(palindrome('hello'));
 console.log(palindrome('madam'));
+console.log(palindrome(1001));
 
 
