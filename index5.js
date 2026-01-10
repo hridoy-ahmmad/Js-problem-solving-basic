65dcbedad281bcb41f0e310329ce0eb0f16d5cd0
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-01-10 16:29:10 +0600

    Remove Duplicates from Array

---
+++ index5.js
@@ -0,0 +1,12 @@
+// Remove Duplicates from Array
+
+function removeDuplicate(arr) {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i])) {
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+console.log(removeDuplicate([1, 1, 2, 2, 3, 3, 4, 4]));
